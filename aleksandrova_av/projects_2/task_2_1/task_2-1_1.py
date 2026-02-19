e29d5fa1e769eb04b7f11af86e5dc61a1928c573
Arina Aleksandrova <arina3624195@gmail.com>
2026-02-19 22:44:28 +0300

    Added task_2-1

---
+++ aleksandrova_av/projects_2/task_2_1/task_2-1_1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 19 17:27:50 2026
+
+@author: Lenovo A
+"""
+
+name = input("Введите имя: ")
+age = input("Возраст: ")
+
+print("Студент", name, sep=" : ", end="!\n")
---
+++ aleksandrova_av/projects_2/task_2_1/task_2-1_2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 19 22:39:01 2026
+
+@author: Lenovo A
+"""
+
+python_version = 3.10
+
+print(f"Language:\tPython\nVersion:\t{python_version}")
