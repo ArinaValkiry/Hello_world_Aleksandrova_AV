ff55cd929c00c11443f7deb95c7778e0fd3185f8
Arina Aleksandrova <arina3624195@gmail.com>
2026-02-19 18:03:57 +0300

    Added folder aleksandrova_av

---
+++ aleksandrova_av/projects_2/task_2_0/task_2-0_3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 19 17:14:39 2026
+
+@author: Lenovo A
+"""
+
+print(4, 2, 2026, sep=".")
---
+++ aleksandrova_av/projects_2/task_2_0/task_2-0_6.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 19 17:17:21 2026
+
+@author: Lenovo A
+"""
+
+f = open("output.txt", "w", encoding="utf-8")
+print("Aleksandrova Arina", file=f)
+f.close()
