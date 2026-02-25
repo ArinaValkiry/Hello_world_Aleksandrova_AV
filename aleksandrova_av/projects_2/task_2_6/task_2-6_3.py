8cde047937b70155d233cf2d7a86e0f2973aa865
Arina Aleksandrova <arina3624195@gmail.com>
2026-02-25 08:30:46 +0300

    Modified files in task_2_6

--- aleksandrova_av/projects_2/task_2_6/task_2-6_3.py
+++ aleksandrova_av/projects_2/task_2_6/task_2-6_3.py
@@ -5,8 +5,11 @@ Created on Sun Feb 22 23:36:36 2026
 @author: arina
 """
 
-donor = input("Введите группу крови донора (I, II, III, IV): ")
-recepient = input("Введите группу крови пациента (I, II, III, IV): ")
+inp_donor = input("Введите группу крови донора (I, II, III, IV): ")
+inp_recepient = input("Введите группу крови пациента (I, II, III, IV): ")
+
+donor = inp_donor.upper()
+recepient = inp_recepient.upper()
 
 if donor == recepient or donor == "I" or recepient == "IV":
     print("Переливание крови возможно")
